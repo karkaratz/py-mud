b8b9124c29c1abc3de30eb913627279021626c2c
Karkaratz <pixelface@techie.com>
2024-01-29 23:37:24 +0100

    Added Beta Version of Room and Item Class

--- common.py
+++ common.py
@@ -16,9 +16,8 @@ def help_menu(ssh_handler):
     ssh_handler.send_message("")
     ssh_handler.send_message("--------------Help needed!!!----------------")
     ssh_handler.send_message("Here a list of commands that you can used during the game.")
-    ssh_handler.send_message(
-        "look -  To observe the room. "
-        "look object/enemy/character - Will provide information about an object, enemy or character.")
+    ssh_handler.send_message( "look -  To observe the room."
+                              "look object/enemy/character - Will provide information about an object, enemy or character.")
     ssh_handler.send_message("inventory - Will show the list of items you have in your inventory.")
     ssh_handler.send_message("go north/south/east/west - To go in a specific direction.")
     ssh_handler.send_message("exit - You need to be in a safe place to exit. It will save your progress and quit the game.")
